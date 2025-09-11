f3d8aaa0eb2c20beeb515ff731cab8e152ad2f42
i1yaya <2665432168910zxcVBN>
2025-09-11 10:39:03 +0300

    Initial Commit

---
+++ ChickensRun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChickensRun
+//
+//  Created by Ilya Ruyatkin on 11.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
